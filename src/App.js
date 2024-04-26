dccb5f6f1fe243fe51c51ac0102ba68ce348ab8b
Belinda Caylor <belinda.caylor@gmail.com>
2024-04-26 14:45:55 -0400

    Add functionality

--- src/App.js
+++ src/App.js
@@ -8,39 +8,47 @@ import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
+import CurrencySelector from './components/CurrencySelector';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
         <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
+            <div className='container text-light m-3 p-4' style={{ background: '#333333' }}>
+                <h1 className='m-3'>Company's Budget Allocation</h1>
+                    <div className='d-flex mt-3'>
                         {
                             // Budget component
-                            <div className='col-sm'>
+                            <div className='flex-grow-1'>
                                 <Budget />
                             </div>
                         }        
 
                         {
                             //Remaining component
-                            <div className='col-sm'>
+                            <div className='flex-grow-1'>
                                 <Remaining />
                             </div>
                         }        
 
                         {
                             //ExpenseTotal component
-                            <div className='col-sm'>
+                            <div className='flex-grow-1'>
                                 <ExpenseTotal />
                             </div>
-                        }        
+                        }
+
+                        {
+                            //Currency component
+                            <div className='flex-grow-1'>
+                                <CurrencySelector />
+                            </div>
+                        }     
                     </div>
-                    <div className='row mt-3'>
+                    <div className='row mt-3 p-3 bg-light'>
                         {
                             //ExpenseList component
-                            <div className='col-sm'>
+                            <div className='flex-grow-1'>
                                 <ExpenseList />
                             </div>
                         }         
@@ -48,7 +56,7 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             //AllocationForm component
-                            <div className='col-sm'>
+                            <div className='flex-grow-1'>
                                 <AllocationForm />
                             </div>
                         }        
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,33 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency, expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
     const handleBudgetChange = (event) => {
+        let budgetLimit = 20000;
+        if(event.target.value > budgetLimit) {
+            alert("The budget cannot exceed "+ currency + budgetLimit);
+            setNewBudget(budgetLimit);
+            return;
+        }
+        if(event.target.value < totalExpenses) {
+            alert("The budget cannot be lower then the spending "+ currency + totalExpenses);
+            setNewBudget(totalExpenses);
+            return;
+        }
         setNewBudget(event.target.value);
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span style={{ marginRight: '3px' }}>Budget: {currency + budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
 export default Budget;
